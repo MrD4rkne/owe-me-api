db72326a8ca50e7ca1198e921d179c1ae9ab1f98
mszopa <marcinsz.pv@gmail.com>
2025-06-22 16:04:21 +0200

    feat: implement GuidHelper for unique GUID generation

---
+++ src/OweMe.Tests.Common.Tests/GuidHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace OweMe.Tests.Common;
+
+public class GuidHelperTests
+{
+    [Test]
+    public void CreateDifferentGuid_ShouldReturnDifferentGuid()
+    {
+        var existingGuid = Guid.NewGuid();
+        var newGuid = GuidHelper.CreateDifferentGuid(existingGuid);
+        newGuid.ShouldNotBe(existingGuid);
+    }
+
+    [Test]
+    public void CreateDifferentGuid_ShouldThrowExceptionAfterMaxAttempts()
+    {
+        var existingGuid = Guid.NewGuid();
+        Assert.Throws<InvalidOperationException>(() => GuidHelper.CreateDifferentGuid(existingGuid, 0));
+    }
+}
---
+++ src/OweMe.Tests.Common/GuidHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace OweMe.Tests.Common;
+
+public static class GuidHelper
+{
+    public static Guid CreateDifferentGuid(Guid other, int maxAttempts = 100)
+    {
+        if (maxAttempts <= 0)
+            throw new ArgumentException("maxAttempts must be greater than 0", nameof(maxAttempts));
+
+        for (int i = 0; i < maxAttempts; i++)
+        {
+            var newGuid = Guid.NewGuid();
+            if (newGuid != other)
+                return newGuid;
+        }
+
+        throw new InvalidOperationException("Unable to generate a unique Guid after maximum attempts.");
+    }
+}
