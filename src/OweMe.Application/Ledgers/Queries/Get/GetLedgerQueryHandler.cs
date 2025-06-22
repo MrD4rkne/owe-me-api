6313a35a8839493273c7d0cc06b9c961fb50bc82
mszopa <marcinsz.pv@gmail.com>
2025-06-22 15:22:17 +0200

    feat: enhance dependency injection for persistence and add ledger retrieval functionality

---
+++ src/OweMe.Application/Ledgers/Queries/Get/GetLedgerQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OweMe.Application.Common;
+using OweMe.Domain.Ledgers;
+using OweMe.Domain.Ledgers.Queries.Get;
+
+namespace OweMe.Application.Ledgers.Commands.Create;
+
+public class GetLedgerQueryHandler(ILedgerContext context, IUserContext userContext) : IRequestHandler<GetLedgerQuery, Result<LedgerDTO>>
+{
+    public async Task<Result<LedgerDTO>> Handle(GetLedgerQuery request, CancellationToken cancellationToken)
+    {
+        var ledger = await context.Ledgers.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+        if (ledger is null || !ledger.CanUserAccess(userContext.Id))
+        {
+            return Result<LedgerDTO>.Failure(LedgerErrors.LedgerNotFound);
+        }
+
+        return Result<LedgerDTO>.Success(LedgerDTO.FromDomain(ledger));
+    }
+}
--- src/OweMe.Persistence/DependencyInjection.cs
+++ src/OweMe.Persistence/DependencyInjection.cs
@@ -10,7 +10,7 @@ namespace OweMe.Persistence;
 [ExcludeFromCodeCoverage]
 public static class DependencyInjection
 {
-    public static void AddPersistence(this IHostApplicationBuilder builder, string connectionString)
+    public static Task AddPersistence(this IHostApplicationBuilder builder, string connectionString, bool shouldRunMigrations = false)
     {
         ArgumentNullException.ThrowIfNull(builder);
         ArgumentNullException.ThrowIfNullOrEmpty(connectionString);
@@ -22,5 +22,19 @@ public static class DependencyInjection
         });
 
         builder.Services.AddScoped<ILedgerContext, LedgerDbContext>();
+
+        return builder.TryRunMigrations(shouldRunMigrations);
+    }
+    
+    private static async Task TryRunMigrations(this IHostApplicationBuilder builder, bool shouldRunMigrations)
+    {
+        if (!shouldRunMigrations)
+        {
+            return;
+        }
+
+        using var scope = builder.Services.BuildServiceProvider().CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<LedgerDbContext>();
+        await context.Database.MigrateAsync();
     }
 }
