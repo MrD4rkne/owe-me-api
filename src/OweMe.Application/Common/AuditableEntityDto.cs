cf6f6ded6336818ee99109e0d0caf2b265471f37
mszopa <marcinsz.pv@gmail.com>
2025-06-26 13:11:02 +0200

    feat: make CreatedAt, CreatedBy, Id, Name, and Description properties required in DTOs

--- src/OweMe.Application/Common/AuditableEntityDto.cs
+++ src/OweMe.Application/Common/AuditableEntityDto.cs
@@ -2,11 +2,11 @@
 
 public record AuditableEntityDto
 {
-    public DateTimeOffset CreatedAt { get; init; }
+    public required DateTimeOffset CreatedAt { get; init; }
 
     public DateTimeOffset? UpdatedAt { get; init; }
 
-    public Guid CreatedBy { get; init; }
+    public required Guid CreatedBy { get; init; }
 
     public Guid? UpdatedBy { get; init; }
 }
--- src/OweMe.Application/Ledgers/LedgerDto.cs
+++ src/OweMe.Application/Ledgers/LedgerDto.cs
@@ -4,11 +4,11 @@ namespace OweMe.Domain.Ledgers;
 
 public record LedgerDto : AuditableEntityDto
 {
-    public Guid Id { get; init; }
+    public required Guid Id { get; init; }
 
-    public string Name { get; init; }
+    public required string Name { get; init; }
 
-    public string Description { get; init; }
+    public required string Description { get; init; }
 
     public Ledger FromDTO()
     {
