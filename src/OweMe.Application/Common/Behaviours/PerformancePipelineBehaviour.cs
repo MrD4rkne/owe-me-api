1aa147e6eef1e37ba30da8c85553fb46ae003014
Marcin Szopa <mszo@foss.pl>
2025-07-16 18:44:56 +0200

    feat: refactor PerformancePipelineBehaviour to use lowercase parameter for maximum elapsed time

--- src/OweMe.Application/Common/Behaviours/PerformancePipelineBehaviour.cs
+++ src/OweMe.Application/Common/Behaviours/PerformancePipelineBehaviour.cs
@@ -6,7 +6,7 @@ namespace OweMe.Application.Common.Behaviours;
 
 public class PerformancePipelineBehaviour<TRequest, TResponse>(
     ILogger<PerformancePipelineBehaviour<TRequest, TResponse>> logger,
-    int MaximumElapsedMilliseconds = 500) : IPipelineBehavior<TRequest, TResponse>
+    int maximumElapsedMilliseconds = 500) : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
@@ -26,11 +26,11 @@ public class PerformancePipelineBehaviour<TRequest, TResponse>(
             logger.LogInformation("Handled {RequestName} in {ElapsedMilliseconds} ms", requestName,
                 stopwatch.ElapsedMilliseconds);
 
-            if (stopwatch.ElapsedMilliseconds > MaximumElapsedMilliseconds)
+            if (stopwatch.ElapsedMilliseconds > maximumElapsedMilliseconds)
             {
                 logger.LogWarning(
                     "Performance warning: {RequestName} took {ElapsedMilliseconds} ms, exceeding the threshold of {Threshold} ms",
-                    requestName, stopwatch.ElapsedMilliseconds, MaximumElapsedMilliseconds);
+                    requestName, stopwatch.ElapsedMilliseconds, maximumElapsedMilliseconds);
             }
         }
     }
