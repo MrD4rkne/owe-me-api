54f7bf804b5282c44bdfe3a63b718c6135ca8652
mszopa <marcinsz.pv@gmail.com>
2025-08-10 15:44:15 +0200

    feat: add nullability checks for logger in LoggingDelegatingHandler

--- api/tests/OweMe.Api.SmokeTests/Configuration/LoggingDelegatingHandler.cs
+++ api/tests/OweMe.Api.SmokeTests/Configuration/LoggingDelegatingHandler.cs
@@ -4,24 +4,24 @@ namespace OweMe.Api.SmokeTests;
 
 public class LoggingDelegatingHandler : DelegatingHandler
 {
-    private readonly ILogger _logger = TestContext.Current.TestOutputHelper.ToLogger<LoggingDelegatingHandler>();
+    private readonly ILogger? _logger = TestContext.Current.TestOutputHelper.ToLogger<LoggingDelegatingHandler>();
     
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
         CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Request: {Method} {Uri}", request.Method, request.RequestUri);
+        _logger?.LogInformation("Request: {Method} {Uri}", request.Method, request.RequestUri);
         if (request.Content != null)
         {
             string requestBody = await request.Content.ReadAsStringAsync(cancellationToken);
-            _logger.LogInformation("Request Body: {Body}", requestBody);
+            _logger?.LogInformation("Request Body: {Body}", requestBody);
         }
 
         var response = await base.SendAsync(request, cancellationToken);
 
-        _logger.LogInformation("Response: {StatusCode} {ReasonPhrase}", response.StatusCode, response.ReasonPhrase);
+        _logger?.LogInformation("Response: {StatusCode} {ReasonPhrase}", response.StatusCode, response.ReasonPhrase);
         
         string responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
-        _logger.LogInformation("Response Body: {Body}", responseBody);
+        _logger?.LogInformation("Response Body: {Body}", responseBody);
 
         return response;
     }
